3c2e0025e06ce99cc6855065e4a7b9c1a4a711b8
Ajda Pretnar <ajdapretnar@gmail.com>
2020-06-02 12:47:13 +0200

    Timeseries alignment script

---
+++ scripts/Timeseries/timeseries-alignment.py
@@ -0,0 +1,18 @@
+"""
+input: Timeseries data.
+output: Timeseries aligned at value n. 
+requires: /
+use case: COVID-19 data
+"""
+
+import numpy as np
+from copy import deepcopy
+
+n = 100
+out_data = deepcopy(in_data)
+out_data.X[out_data.X < n] = 0
+ar = np.argwhere(out_data.X)
+
+cols, shifts = np.unique(ar[:, 1], return_counts=True)
+out_data.X = np.array([np.roll(out_data.X[:, col], shift) for col, shift in zip(cols, shifts)]).T
+out_data.X[out_data.X==0]= np.nan
